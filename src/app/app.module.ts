31d52abddaa9e792562fa9169de2d5a4482c5add
vali-ionescu <valentin.ionescu@cst.ro>
2023-12-21 13:55:08 +0200

    Add login and register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './src/app/auth/login/login.component';
+import { RegisterComponent } from './src/app/auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
